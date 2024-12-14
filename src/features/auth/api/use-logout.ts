0df91cb5c78b52242c5b6891c2e8ac51df10174a
Levin Bänninger <l.baenninger@icloud.com>
2024-12-14 01:20:16 +0100

    feature: implement create workspace functionality

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -2,6 +2,7 @@ import { client } from '@/lib/rpc';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { InferResponseType } from 'hono';
 import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)['$post']>;
 
@@ -12,11 +13,20 @@ export const useLogout = () => {
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth.logout.$post();
+
+      if (!response.ok) {
+        throw new Error('Failed to log out');
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success('Logged out successfully');
       router.refresh();
       queryClient.invalidateQueries({ queryKey: ['current'] });
+    },
+    onError: () => {
+      toast.error('Failed to log out');
     },
   });
 
