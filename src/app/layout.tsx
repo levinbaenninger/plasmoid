542adf75f11bc7ee1a56c1ead3545d0cb5c944bb
Levin Bänninger <l.baenninger@icloud.com>
2024-12-16 21:42:53 +0100

    feature: update metadata title and add custom icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,11 @@ const inter = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'PLASMOID',
+  title: 'Plasmoid',
   description: 'Harness the power of productivity',
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>',
+  },
 };
 
 export default function RootLayout({
